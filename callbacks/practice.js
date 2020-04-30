6d186c4f28bcbab4f6ffc1186d76745d955ce1cf
rsarkis23 <learning.r2@gmail.com>
2020-04-30 17:57:23 -0500

    sumbitting the callbacks' practise.js code

--- callbacks/practice.js
+++ callbacks/practice.js
@@ -28,6 +28,10 @@
 */
 
 // Code Here 
+function first(arr, cb){
+  return cb(arr[0])
+}
+
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +52,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(arr, cb){
+  return cb(arr[arr.length-1])
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,6 +73,9 @@ last(names, function(lastName){
 */
 
 //Code Here
+function multiply(num1, num2, cb){
+  return cb(num1*num2)
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +95,14 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(arr, aName, cb){
+  if (arr.indexOf(aName) >= 0){
+    return cb(true)
+  }else{
+    return cb(false)
+  }
+}
+
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,6 +124,16 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq(arr, cb){
+  let newArr = []
+  for (let i = 0; i < arr.length; i++){
+    if (arr.indexOf(arr[i])=== i){
+      newArr.push(arr[i])
+    }
+  }
+  return cb(newArr)
+
+}
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -123,6 +151,11 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+function each(arr, cb){
+  for (let i = 0; i < arr.length; i++){
+    cb(arr[i], i)
+  }
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -140,6 +173,15 @@ each(names, function(item, indice){
 */
 
 // Code here
+function getUserById(users, id, cb){
+  for (let i = 0; i < users.length; i++){
+    if (id === users[i].id){
+      return cb(users[i])
+    }
+  }
+  
+
+}
 
 // Do not edit the code below.
 var users = [
